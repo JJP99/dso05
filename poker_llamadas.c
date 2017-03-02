0005db02d66e4fe1e3f4d879b70b1da6f42be1e2
Andres Rodriguez <andres@uma.es>
2017-03-02 20:13:28 +0000

    poker_llamadas mejorado

--- poker_llamadas.c
+++ poker_llamadas.c
@@ -9,7 +9,8 @@
 main()
 {
 int res;
-char * mensaje="Salida write con int 80h desde asm\n";  /* tamaño 35 caracteres */
+char * mensaje1="Salida write con int 80h desde asm\n";  /* tamaño 35 caracteres */
+char * mensaje2="Salida write con SWI/SVC desde asm\n";  /* tamaño 35 caracteres */
 
 /*Pinta por pantalla usando la librería de C stdio.h*/
 printf("Salida printf() desde C\n");
@@ -23,19 +24,29 @@ write(STDOUT_FILENO, "Salida write() desde C\n", 23);   /* STDOUT_FILENO = 1 */
 res=syscall(SYS_write, STDOUT_FILENO, "Salida syscall() desde C\n", 25);  // SYS_write = 1
 
 #if defined(__x86_64__) || defined(__i386__)  // Intel x32 & x64
-// por ultimo usa ensamblador para hacer la llamada al sistema con la interrupción 80 hex 
-asm("mov $4,  %%eax \n\t"
+// usa ensamblador para hacer la llamada al sistema con la interrupción 80 hex 
+asm("mov $1,  %%eax \n\t"
     "mov $1,  %%ebx \n\t" 
-    "mov %k0,  %%ecx \n\t" 
+    "mov %k0, %%ecx \n\t" 
     "mov $35, %%edx \n\t"
     "int $0x80 \n\t" 
  	: // sin salida
-	: "r" (mensaje)  // entrada mensaje en %0 para lectura 
+	: "r" (mensaje1)  // entrada mensaje en %0 para lectura 
 	: "%eax", "%ebx", "%ecx", "%edx"   // registros usados para que el compilador lo sepa 
     );
 #endif
 
 #ifdef __arm__  // ARM
+// usa ensamblador para hacer la llamada al sistema con la interrupción  swi/svc 
+asm( "mov r7 , #4 \n\t"   
+     "mov r2 , #35 \n\t"   
+     "mov r1 , %0 \n\t"   
+     "mov r0 , #1 \n\t"   
+     "svc 0\n\t"
+	:
+	: "r" (mensaje2)
+        : "r0", "r1", "r2", "r7"
+);
 
 #endif
 
