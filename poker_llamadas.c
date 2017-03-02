d8d0221f75b32fdfac09a41fe73e979ad4ce32d3
Andrés Rodríguez <andres@uma.es>
2017-03-02 15:13:22 +0100

    poker_llamadas corregido

--- poker_llamadas.c
+++ poker_llamadas.c
@@ -4,10 +4,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+#include <sys/syscall.h>   /* For SYS_xxx definitions */
 
 main()
 {
+int res;
 char * mensaje="Salida write con int 80h desde asm\n";  /* tamaño 35 caracteres */
 
 /*Pinta por pantalla usando la librería de C stdio.h*/
@@ -19,14 +20,17 @@ printf("Salida printf() desde C\n");
 write(STDOUT_FILENO, "Salida write() desde C\n", 23);   /* STDOUT_FILENO = 1 */
 
 /* ahora pinta usando la funcion de C en unistd.h que llama al sistema de forma genérica*/
-#define __NR_write  4   /* la llamada al sistema write es la 4 */
-syscall(__NR_write, STDOUT_FILENO, "Salida syscall() desde C\n", 25);
+res=syscall(SYS_write, STDOUT_FILENO, "Salida syscall() desde C\n", 25);  // SYS_write = 1
 
-/* por ultimo usa ensamblador para hacer la llamada al sistema con la interrupción 80 hex */
-asm("mov $4,%%eax; mov $1,%%ebx; mov %0,%%ecx; mov $35,%%edx; int $0x80;" 
- 	: /* sin salida */
-	: "r" (mensaje)  /* entrada mensaje en %0 para lectura */
-	: "%eax", "%ebx", "%ecx", "%edx"   /* registros usados para que el compilador lo sepa */
+// por ultimo usa ensamblador para hacer la llamada al sistema con la interrupción 80 hex 
+asm("mov $4,  %%eax \n\t"
+    "mov $1,  %%ebx \n\t" 
+    "mov %k0,  %%ecx \n\t" 
+    "mov $35, %%edx \n\t"
+    "int $0x80 \n\t" 
+ 	: // sin salida
+	: "r" (mensaje)  // entrada mensaje en %0 para lectura 
+	: "%eax", "%ebx", "%ecx", "%edx"   // registros usados para que el compilador lo sepa 
     );
 
 exit(0);
