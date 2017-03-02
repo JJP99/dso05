25d48759495dbc840735d7dbd300fb96fce3f341
Andres Rodriguez <andres@uma.es>
2017-03-02 19:09:12 +0000

    working...

--- poker_llamadas.c
+++ poker_llamadas.c
@@ -22,6 +22,8 @@ write(STDOUT_FILENO, "Salida write() desde C\n", 23);   /* STDOUT_FILENO = 1 */
 /* ahora pinta usando la funcion de C en unistd.h que llama al sistema de forma genérica*/
 res=syscall(SYS_write, STDOUT_FILENO, "Salida syscall() desde C\n", 25);  // SYS_write = 1
 
+#ifdef __i386__
+printf("Compilando para Intel..\n");
 // por ultimo usa ensamblador para hacer la llamada al sistema con la interrupción 80 hex 
 asm("mov $4,  %%eax \n\t"
     "mov $1,  %%ebx \n\t" 
@@ -32,6 +34,12 @@ asm("mov $4,  %%eax \n\t"
 	: "r" (mensaje)  // entrada mensaje en %0 para lectura 
 	: "%eax", "%ebx", "%ecx", "%edx"   // registros usados para que el compilador lo sepa 
     );
+#endif
+
+#ifdef __arm__
+printf("Compilando para ARM..\n");
+
+#endif
 
 exit(0);
 
